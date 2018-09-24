d8887fef38d18db6fa74c5b966bafee5b6925070
Travis Desir <travisgdesir31@gmail.com>
2018-09-24 00:49:12 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,13 +1,13 @@
 require 'pry'
 def reformat_languages(languages)
- haxh = {}
+new_hash = {}
   languages.each do |style,language|
     language.each do |name,type|
-        if haxh[name] == nil
-          haxh[name] = type
-          haxh[name][:style] = []
+        if new_hash[name] == nil
+          new_hash[name] = type
+          new_hash[name][:style] = []
         end
-        haxh[name][:style] << style
+        new_hash[name][:style] << style
     end
   end
   new_hash
